286420fd7bebb55a14ab065c405a1dbf56472d16
Pascal Kray <57595818+krapas170@users.noreply.github.com>
2023-03-06 21:02:15 +0100

    Update extract_commands.sh

--- build/extract_commands.sh
+++ build/extract_commands.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
 
-# Erstellt ein Array mit allen Bash-Skripten im Repository
-scripts=($(find . -type f -name "*.sh"))
+# Suche nach Bash-Skripten im Repository und extrahiere Befehle
+for script in $(find . -type f -name "*.sh"); do
+  # Extrahiere den Skriptnamen aus dem Pfad
+  name=$(basename "$script" .sh)
 
-# Erstellt eine Markdown-Datei und schreibt die Überschrift
-echo "# Liste der Befehle" > commands.md
+  # Schreibe den Skriptnamen in die Markdown-Datei
+  echo "## $name" >> commands.md
 
-# Schleife durch alle Bash-Skripte
-for script in "${scripts[@]}"
-do
-  # Überprüfen, ob das Skript ausführbar ist
-  if [[ -x "$script" ]]; then
-    # Extrahieren des Titels aus dem Skriptnamen
-    title=$(basename "$script" .sh)
+  # Suche nach dem Befehl im Skript und schreibe ihn in die Markdown-Datei
+  grep "^# Execute command: " "$script" | sed "s/^# Execute command: /- \`/" | sed "s/\$/\`/" >> commands.md
 
-    # Extrahieren aller Befehle aus dem Skript
-    commands=$(grep -E "^# Execute command: " "$script" | sed -e "s/# Execute command: //")
-
-    # Fügt den Titel und die Befehle der Markdown-Datei hinzu
-    echo "## $title" >> commands.md
-    echo "\`\`\`" >> commands.md
-    echo "$commands" >> commands.md
-    echo "\`\`\`" >> commands.md
-  fi
+  # Füge eine Leerzeile zwischen den Skripten hinzu
+  echo "" >> commands.md
 done
+
+# Gebe die extrahierten Befehle aus
+echo "Extrahierte Befehle:"
+cat commands.md
+
+# Lade die Markdown-Datei als Artefakt hoch
+echo "Lade commands.md als Artefakt hoch..."
+echo "::set-output name=command-file::commands.md"
