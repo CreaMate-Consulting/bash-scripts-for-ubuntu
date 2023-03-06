db8ae7463ac84efd46e691379204ae8bb950ea32
Pascal Kray <57595818+krapas170@users.noreply.github.com>
2023-03-06 20:40:29 +0100

    Update extract_commands.sh

--- build/extract_commands.sh
+++ build/extract_commands.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 
-# Suche nach allen Bash-Skripten im Repository
-files=$(find . -name "*.sh")
+# Erstellt ein Array mit allen Bash-Skripten im Repository
+scripts=($(find . -type f -name "*.sh"))
 
-# Durchsuche jedes Skript nach Befehlen und formatiere sie als Markdown
-for file in $files; do
-  commands=$(grep -E "# Execute command:.*" "$file" | sed 's/# Execute command:    //g')
-  if [ ! -z "$commands" ]; then
-    echo "### $(basename "$file")" >> commands.md
-    echo "\`\`\`bash" >> commands.md
+# Erstellt eine Markdown-Datei und schreibt die Überschrift
+echo "# Liste der Befehle" > commands.md
+
+# Schleife durch alle Bash-Skripte
+for script in "${scripts[@]}"
+do
+  # Überprüfen, ob das Skript ausführbar ist
+  if [[ -x "$script" ]]; then
+    # Extrahieren des Titels aus dem Skriptnamen
+    title=$(basename "$script" .sh)
+
+    # Extrahieren aller Befehle aus dem Skript
+    commands=$(grep -E "^# Execute command: " "$script" | sed -e "s/# Execute command: //")
+
+    # Fügt den Titel und die Befehle der Markdown-Datei hinzu
+    echo "## $title" >> commands.md
+    echo "\`\`\`" >> commands.md
     echo "$commands" >> commands.md
     echo "\`\`\`" >> commands.md
-    echo "" >> commands.md
   fi
 done
