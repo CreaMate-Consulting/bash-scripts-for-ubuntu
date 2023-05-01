820c3b8e6965958e4468303912bed006cd7941b7
Pascal Kray <57595818+krapas170@users.noreply.github.com>
2023-05-01 16:14:59 +0200

    Create wireguard-client_ubuntu22.04.sh

---
+++ ubuntu22.04/Wireguard/wireguard-client_ubuntu22.04.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+#
+# Name:           wireguard-client_ubuntu22.04.sh
+# Description:    This script installs Wireguard-Client on a Linux Ubuntu 22.04 system.
+# Author:         OpenAI ChatGPT
+# GitHub URI:     https://github.com/openai/
+# License:        GPL v3 or later
+# License URI:    https://www.gnu.org/licenses/gpl-3.0.de.html
+#
+# Execute command: wget "https://raw.githubusercontent.com/CreaMate-Consulting/bash-scripts-for-ubuntu/main/ubuntu22.04/Wireguard/wireguard-client_ubuntu22.04.sh" && bash wireguard-client_ubuntu22.04.sh
+
+#
+
+# Check if the user running the script is root
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root"
+   exit 1
+fi
+
+#Update package list
+apt update
+
+# Add the WireGuard PPA
+sudo add-apt-repository ppa:wireguard/wireguard -y
+
+# Update the system
+sudo apt update -y && sudo apt upgrade -y
+
+# Install WireGuard
+sudo apt install wireguard -y
+
+# Generate a new key pair for the client
+umask 077
+wg genkey | tee privatekey | wg pubkey > publickey
+
+# Get the server's public key and IP address
+read -p "Enter the server's public key: " server_public_key
+read -p "Enter the server's IP address: " server_ip
+
+# Create the WireGuard configuration file
+sudo tee /etc/wireguard/wg0.conf > /dev/null <<EOT
+[Interface]
+PrivateKey = $(cat privatekey)
+Address = 10.0.0.2/24
+
+[Peer]
+PublicKey = $server_public_key
+AllowedIPs = 0.0.0.0/0
+Endpoint = $server_ip:51820
+PersistentKeepalive = 15
+EOT
+
+# Start the WireGuard client
+sudo wg-quick up wg0
+
+# Show the WireGuard interface status
+sudo wg show
